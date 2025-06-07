4a745a5b612055f5d6bf5fd4b44134c28e69e4fe
Shalu Chauhan <shalu859582@gmail.com>
2025-06-08 00:02:38 +0530

    Updated

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -5,7 +5,7 @@ let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
 
-ppublic_users.post("/register", (req, res) => {
+public_users.post("/register", (req, res) => {
   const { username, password } = req.body;
 
   // Check if username and password are provided
